c0dfeaf8b3f57a9937853519454fd8002b40c665
sbx_user1051 <sbx_user1051@169.254.22.202>
2024-12-19 17:19:31 +0000

    Thu Dec 19 17:19:31 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,16 +6,22 @@ def checkout(skus):
     prices = {'A':50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
 
     #offers
-    offers = {'A':((3,130)(5,200)), 'B':(2,45), 'E': (2,'B')}
+    offers = {'A':[(3,130)(5,200)], 'B':[(2,45)], 'E': [(2,'B')]}
     
     if not isinstance(skus, str) or any(item not in prices for item in skus):
         return -1
     
     total = 0
+
+    #handle different cases seperately
+    
     for item in 'ABCD':
         count = skus.count(item)
         if item in offers:
+            
+
             offer_qty, offer_price = offers[item]
+            if item == 'A':
             
             total += (count //offer_qty) * offer_price #apply offer here
             total += (count % offer_qty) * prices[item] #remaining 
@@ -23,6 +29,7 @@ def checkout(skus):
             total += count * prices[item]
 
     return total 
+
 
 
 
