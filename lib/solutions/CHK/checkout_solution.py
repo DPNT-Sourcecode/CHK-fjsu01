4763051311087c98a0d8c792d23b06d177b00226
sbx_user1051 <sbx_user1051@169.254.22.202>
2024-12-19 16:59:22 +0000

    Thu Dec 19 16:59:22 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,3 +5,13 @@
 def checkout(skus):
     prices = {'A':50, 'B': 30, 'C': 20, 'D': 15}
 
+    #offers
+    offers = {'A':(3,130), 'B':(2,45)}
+    
+    if not isinstance(skus, str) or any(item not in prices for item in skus):
+        return -1
+    
+    total = 0
+    for item in 
+
+
